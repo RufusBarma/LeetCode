f9671bf42bf938ae4d5760604a2e44cbc7a207ee
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-02-15 14:00:31 +0500

    Init commit

---
+++ LeetCode/ContainsDuplicate_217.cs
@@ -0,0 +1,21 @@
+﻿namespace ContainsDuplicate_217;
+
+public class Solution {
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/contains-duplicate/">Contains duplicate on LeetCode</see>
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var seen = new HashSet<int>();
+        foreach (var num in nums)
+        {
+            if (seen.Contains(num))
+                return true;
+            seen.Add(num);
+        }
+
+        return false;
+    }
+}
