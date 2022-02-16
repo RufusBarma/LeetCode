32970fd735adf50a6197803dc7d8dc3093dfe88e
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-02-16 16:50:19 +0500

    Solve Single Number 136

---
+++ LeetCode/SingleNumber_136.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace SingleNumber_136;
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        return nums.Aggregate(0, (current, num) => current ^ num);
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var solutions = new List<Func<int[], int>>
+        {
+            SingleNumber,
+        };
+        var tests = new List<(int, int[])>()
+        {
+            (1, new[] {1}),
+            (1, new[] {2, 2, 1}),
+            (4, new[] {4,1,2,1,2}),
+        };
+        foreach (var test in tests)
+        foreach (var solution in solutions)
+        {
+            Assert.Equal(test.Item1, solution(test.Item2));
+        }
+    }
+}
