6511641719465c7d3cf253ea6036ee05a5e874f2
YusupovRA <r.a.yusupov@chelindbank.ru>
2022-02-25 11:20:49 +0500

    Add another solution Single Number 136

--- LeetCode/SingleNumber_136.cs
+++ LeetCode/SingleNumber_136.cs
@@ -4,8 +4,83 @@ namespace SingleNumber_136;
 
 public class Solution
 {
+    public struct Pair
+    {
+        public int Sum;
+        public int Max;
+
+        public Pair(int sum, int max)
+        {
+            Sum = sum;
+            Max = max;
+        }
+    }
+    public int solution(string S, int[] C) {
+        var pairs = new List<Pair>();
+        var currentChar = S[0];
+        var count = 0;
+        var sum = 0;
+        var max = 0;
+        for (var i = 0; i < S.Length; i++)
+        {
+            if (currentChar != S[i])
+            {
+                if (count != 0)
+                    pairs.Add(new Pair(sum, max));
+                count = 0;
+                sum = 0;
+                max = 0;
+                currentChar = S[i];
+            }
+            else
+                count++;
+            sum += C[i];
+            max = C[i] > max ? C[i] : max;
+        }
+        if (count > 0)
+            pairs.Add(new Pair(sum, max));
+
+        return pairs.Sum(pair => pair.Sum - pair.Max);
+    }
+
+    [Fact]
+    public void TestSol()
+    {
+        var s = "aaaa";
+        var c = new[] {3, 4, 5, 6};
+        Assert.Equal(12, solution(s, c));
+    }
+    
+    public int solution3(int[] A) {
+        int N = A.Length;
+        int result = 0;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (A[i] == A[j])
+                    result = Math.Max(result, Math.Abs(i - j));
+        return result;
+    }
+ 
+    public int solution2(int[] A) {
+        var counter= new Dictionary<int,int>();
+
+        for(var i=0; i<A.Length;i++)
+        {
+            if (!counter.TryAdd(A[i], 1))
+                counter[A[i]]++;
+        }
+        var g = counter.OrderByDescending(pair => pair.Key).FirstOrDefault(pair => pair.Key == pair.Value).Key;
+        foreach (var pair in counter.OrderByDescending(pair => pair.Key))
+        {
+            if (pair.Key == pair.Value)
+                return pair.Key;
+        }
+        
+        return -1;
+    }
     public int SingleNumber(int[] nums)
     {
+        // var g = Enumerable.Repeat(0, A.Length).ToList();
         return nums.Aggregate(0, (current, num) => current ^ num);
     }
 
