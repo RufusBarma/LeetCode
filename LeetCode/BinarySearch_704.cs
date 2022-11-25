185bec199fb25b6ca9e07cd6def58bec94765977
RufusBarma <neymanyu@gmail.com>
2022-11-25 22:16:15 +0500

    Solve Binary Search 704
    https://leetcode.com/problems/binary-search/

---
+++ LeetCode/BinarySearch_704.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace BinarySearch_704;
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left <= right)
+        {
+            var middle = (right + left) / 2;
+            var middleNum = nums[middle];
+            if (middleNum == target)
+                return middle;
+            if (middleNum < target)
+                left = middle +1;
+            else if (middleNum > target)
+                right = middle -1;
+        }
+        return -1;
+    }
+    
+    [Fact]
+    public void Test()
+    {
+        var tests = new List<(int expect, (int[] nums, int target) args)>
+        {
+            (4, (new[] {-1,0,3,5,9,12}, 9)),
+            (-1, (new[] {-1,0,3,5,9,12}, 2)),
+            (0, (new[] {5}, 5)),
+        };
+        foreach (var test in tests)
+            Assert.Equal(test.expect, Search(test.args.nums, test.args.target));
+    }
+}
