61fb47ecbb833ebe19e6791c47b948130275714a
RufusBarma <neymanyu@gmail.com>
2022-11-25 16:30:54 +0500

    Solve Linked List Cycle 141

---
+++ LeetCode/LinkedListCycle_141.cs
@@ -0,0 +1,23 @@
+﻿namespace LinkedListCycle_141;
+
+public class ListNode {
+  public int val;
+  public ListNode? next;
+  public ListNode(int x) {
+      val = x;
+      next = null;
+  }
+}
+
+public class Solution {
+    public bool HasCycle(ListNode? head)
+    {
+        while (head?.next != null)
+        {
+            (head.next, head) = (head, head.next);
+            if (head == head.next)
+                return true;
+        }
+        return false;
+    }
+}
