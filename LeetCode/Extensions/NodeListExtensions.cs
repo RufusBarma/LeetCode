59d632b88c71f7e476ffba9069a82686567895c2
RufusBarma <neymanyu@gmail.com>
2022-11-25 20:40:11 +0500

    Solve Reverse Linked List 206
    https://leetcode.com/problems/reverse-linked-list/

--- LeetCode/Extensions/NodeListExtensions.cs
+++ LeetCode/Extensions/NodeListExtensions.cs
@@ -4,8 +4,8 @@ namespace LeetCode.Extensions;
 
 public class ListNode { 
     public int val;
-    public ListNode next;
-    public ListNode(int val=0, ListNode next=null) {
+    public ListNode? next;
+    public ListNode(int val=0, ListNode? next=null) {
         this.val = val;
         this.next = next;
     }
---
+++ LeetCode/ReverseLinkedList_206.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Extensions;
+
+namespace ReverseLinkedList_206;
+
+public class Solution {
+    public ListNode? ReverseList(ListNode? head)
+    {
+        ListNode? previous = null;
+        while (head != null)
+        {
+            var newPrevious = new ListNode(head.val, previous);
+            previous = newPrevious;
+            head = head.next;
+        }
+        return previous;
+    }
+}
