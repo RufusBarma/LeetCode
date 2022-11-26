196f785650e75ebd38819836ae9fe828e4f6bc46
RufusBarma <neymanyu@gmail.com>
2022-11-26 23:28:39 +0500

    Solve "49. Group Anagrams"
    https://leetcode.com/problems/group-anagrams/

---
+++ LeetCode/49_GroupAnagrams.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace GroupAnagrams_49;
+
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var hashStrs = new Dictionary<string, IList<string>>();
+        foreach (var word in strs)
+        {
+            var chars = new string(word.OrderBy(c => c).ToArray());
+            hashStrs.TryAdd(chars, new List<string>());
+            hashStrs[chars].Add(word);
+        }
+        return hashStrs.Select(anagram => anagram.Value).ToList();
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var tests = new List<(IList<IList<string>> expect, string[] strs)>
+        {
+            (new[]
+            {
+                new[]{ "bat" },
+                new[]{"nat","tan"},
+                new[]{"ate","eat","tea"}
+            }, new[] { "eat","tea","tan","ate","nat","bat" }),
+            (new[]{new[]{ "a" }}, new[] { "a" }),
+            (new[] { new[] { "" } }, new[] { "" })
+        };
+        foreach (var test in tests)
+        {
+            var result = GroupAnagrams(test.strs);
+            Assert.Equal(test.expect, result);
+        }
+    }
+}
