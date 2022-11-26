8bf61f8bedd0d19c0331d15694d4a5198af12feb
RufusBarma <neymanyu@gmail.com>
2022-11-26 20:53:18 +0500

    Solve "15. 3Sum"
    https://leetcode.com/problems/3sum/

---
+++ LeetCode/15_3Sum.cs
@@ -0,0 +1,33 @@
+﻿namespace ThreeSum_15;
+
+public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var output = new List<IList<int>>();
+        nums = nums.OrderBy(i => i).ToArray();
+        for (var iIndex = 0; iIndex < nums.Length; iIndex++)
+        {
+            if (iIndex != 0 && nums[iIndex] == nums[iIndex-1])
+                continue;
+            var jIndex = iIndex + 1;
+            var kIndex = nums.Length - 1;
+            while (jIndex < kIndex)
+            {
+                var sum = nums[iIndex] + nums[jIndex] + nums[kIndex];
+                if (sum == 0)
+                {
+                    output.Add(new List<int>{nums[iIndex], nums[jIndex], nums[kIndex]});
+                    jIndex++;
+                    while (jIndex < kIndex && nums[jIndex] == nums[jIndex - 1])
+                        jIndex++;
+                }
+                else if (sum < 0)
+                    jIndex++;
+                else
+                    kIndex--;
+            }
+        }
+        return output;
+    }
+}
