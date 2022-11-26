1854bf09efed1f4a76f36095f1bb683aca9564be
RufusBarma <neymanyu@gmail.com>
2022-11-26 17:54:20 +0500

    Solve Two Sum 1
    https://leetcode.com/problems/two-sum/

---
+++ LeetCode/TwoSum_1.cs
@@ -0,0 +1,17 @@
+﻿namespace TwoSum_1;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var numsWithIndex = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var another = target - nums[i];
+            if (numsWithIndex.TryGetValue(another, out var anotherIndex))
+                return new []{anotherIndex, i};
+            numsWithIndex.TryAdd(nums[i], i);
+        }
+        return Array.Empty<int>();
+    }
+}
