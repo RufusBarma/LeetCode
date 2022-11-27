c27887494f4f8564cfc035f5e511ac3f8cb30e67
RufusBarma <neymanyu@gmail.com>
2022-11-28 01:01:30 +0500

    Solve "11. Container With Most Water"
    https://leetcode.com/problems/container-with-most-water/

---
+++ LeetCode/11_ContainerWithMostWater.cs
@@ -0,0 +1,20 @@
+﻿namespace ContainerWithMostWater_11;
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        var maxArea = 0;
+        var left = 0;
+        var right = height.Length - 1;
+        while (left < right)
+        {
+            maxArea = Math.Max(maxArea, (right - left) * Math.Min(height[left], height[right]));
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return maxArea;
+    }
+}
