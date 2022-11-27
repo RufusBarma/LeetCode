148e1b468e412552911f3fa5f98809a51a3ffc02
RufusBarma <neymanyu@gmail.com>
2022-11-27 12:45:09 +0500

    Solve "20. Valid Parentheses"
    https://leetcode.com/problems/valid-parentheses/

---
+++ LeetCode/20_ValidParentheses.cs
@@ -0,0 +1,33 @@
+﻿namespace ValidParentheses_20;
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var closeBrackets = new HashSet<char>{')', '}', ']'};
+        var pairs = new Dictionary<char, char>(){
+            {'(', ')'},
+            {'{', '}'},
+            {'[', ']'},
+            {')', '('},
+            {'}', '{'},
+            {']', '['},
+        };
+        var openBrackets = new Stack<char>();
+        foreach(var bracket in s){
+            if (closeBrackets.Contains(bracket)){
+                if (openBrackets.TryPop(out var lastBracket))
+                {
+                    if (pairs[bracket] == lastBracket)
+                        continue;
+                    return false;
+                }else{
+                    return false;
+                }
+            }else{
+                openBrackets.Push(bracket);
+            }
+        }
+        return openBrackets.Count == 0;
+    }
+}
