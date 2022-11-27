40c1f400401d9ff0c49c2f7a369d1f0a41a36e53
RufusBarma <neymanyu@gmail.com>
2022-11-27 23:27:50 +0500

    Solve "692. Top K Frequent Words"
    https://leetcode.com/problems/top-k-frequent-words/

---
+++ LeetCode/692_TopKFrequentWords.cs
@@ -0,0 +1,24 @@
+﻿namespace TopKFrequentWords_692;
+
+public class Solution
+{
+    public IList<string> TopKFrequent(string[] words, int k) {
+        if (words.Length <= k)
+        {
+            Array.Sort(words);
+            return words.AsEnumerable().Distinct().ToArray();
+        }
+        var wordCounts = new Dictionary<string, int>();
+        foreach (var word in words)
+        {
+            wordCounts.TryAdd(word, 0);
+            wordCounts[word]++;
+        }
+        return wordCounts
+            .OrderByDescending(wordCount => wordCount.Value)
+            .ThenBy(wordCount => wordCount.Key)
+            .Take(k)
+            .Select(wordCount => wordCount.Key)
+            .ToList();
+    }
+}
