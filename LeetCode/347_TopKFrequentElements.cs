eb3f4bd21f19240e24835359e5e8c822710261e4
RufusBarma <neymanyu@gmail.com>
2022-11-27 23:39:50 +0500

    Solve "347. Top K Frequent Elements"
    https://leetcode.com/problems/top-k-frequent-elements/

---
+++ LeetCode/347_TopKFrequentElements.cs
@@ -0,0 +1,24 @@
+﻿namespace TopKFrequentElements_347;
+
+public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        if (nums.Length <= k)
+        {
+            Array.Sort(nums);
+            return nums.AsEnumerable().Distinct().ToArray();
+        }
+        var numsCounts = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            numsCounts.TryAdd(num, 0);
+            numsCounts[num]++;
+        }
+        return numsCounts
+            .OrderByDescending(wordCount => wordCount.Value)
+            .Take(k)
+            .Select(wordCount => wordCount.Key)
+            .ToArray();
+    }
+}
