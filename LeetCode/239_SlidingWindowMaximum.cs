d7015069d8e62dc0ed722b8441593fe13fb39f52
RufusBarma <neymanyu@gmail.com>
2022-11-28 17:07:27 +0500

    Solve "239. Sliding Window Maximum"
    https://leetcode.com/problems/sliding-window-maximum/

---
+++ LeetCode/239_SlidingWindowMaximum.cs
@@ -0,0 +1,26 @@
+﻿namespace SlidingWindowMaximum_239;
+
+public class Solution
+{
+    public int[] MaxSlidingWindow(int[] nums, int k)
+    {
+        var result = new List<int>();
+        var maxPoints = new LinkedList<int>();
+        var windowPoints = new Queue<int>();
+
+        foreach (var num in nums)
+        {
+            windowPoints.Enqueue(num);
+            if (windowPoints.Count > k && maxPoints.First!.Value == windowPoints.Dequeue())                
+                maxPoints.RemoveFirst();
+                
+            while (maxPoints.Count != 0 && maxPoints.Last!.Value < num)                
+                maxPoints.RemoveLast();
+
+            maxPoints.AddLast(num);
+            result.Add(maxPoints.First!.Value);
+        }
+
+        return result.Skip(k-1).ToArray();
+    }
+}
