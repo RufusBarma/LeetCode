f20496c2a9706eb6efe41d4a3c1c98d5a721b5f3
RufusBarma <neymanyu@gmail.com>
2022-11-29 16:09:12 +0500

    Solve "110. Balanced Binary Tree"
    https://leetcode.com/problems/balanced-binary-tree/

---
+++ LeetCode/110_BalancedBinaryTree.cs
@@ -0,0 +1,36 @@
+﻿namespace BalancedBinaryTree_110;
+
+public class TreeNode
+{
+    public TreeNode? left;
+    public TreeNode? right;
+    public int val;
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public bool IsBalanced(TreeNode root) 
+    {
+        return CheckLength(root).balanced;
+    }
+    
+    private (bool balanced, int height) CheckLength(TreeNode? root)
+    {
+        if (root == null)
+            return (true, 0);
+
+        var left = CheckLength(root.left);
+        var right = CheckLength(root.right);
+        
+        var diff = Math.Abs(left.height - right.height);
+        var height = 1 + Math.Max(left.height, right.height);
+        
+        return (diff <= 1 && left.balanced && right.balanced, height);
+    }
+}
