56c3dd4300af1e8ecae301557c0f2e90e9d5ce8f
RufusBarma <neymanyu@gmail.com>
2022-11-29 18:26:36 +0500

    Solve "121. Best Time to Buy and Sell Stock"
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ LeetCode/121_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,18 @@
+﻿namespace BestTimeToBuyAndSellStock_121;
+
+// 121. Best Time to Buy and Sell Stock
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        var maxProfit = 0;
+        var globalMin = prices[0];
+        foreach (var price in prices)
+        {
+            if (price < globalMin)
+                globalMin = price;
+            maxProfit = Math.Max(maxProfit, price - globalMin);
+        }
+        return maxProfit;
+    }
+}
