acf9b246857c988f53503ff90cc389d2c05952ba
RufusBarma <neymanyu@gmail.com>
2022-12-02 20:46:39 +0500

    unsolved problems 309, 480, 714

---
+++ LeetCode/309_BestTimeToBuyAndSellStockWithCooldown.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace BestTimeToBuyAndSellStockWithTransactionFee_714;
+
+// TODO 309. Best Time to Buy and Sell Stock with Cooldown
+// public class Solution
+// {
+//     public int MaxProfit(int[] prices, int fee)
+//     {
+//         var lastDay = prices.Length - 1;
+//         var maxProfit = 0;
+//         var buyingDate = 0;
+//         var sellingDate = 0;
+//         for (var today = 1; today < prices.Length; today++)
+//         {
+//             var itsProfit = prices[sellingDate] > prices[today] && prices[buyingDate] - prices[sellingDate] - fee > 0;
+//             // var letsSell = itsProfit || today == lastDay;
+//             if (!itsProfit)
+//                 sellingDate++;
+//             
+//             if (itsProfit)
+//             {
+//                 maxProfit += prices[sellingDate] - prices[buyingDate] - fee;
+//                 buyingDate = today;
+//                 sellingDate = today;
+//             }
+//         }
+//         return maxProfit;
+//     }
+//
+//     [Fact]
+//     public void Test()
+//     {
+//         var tests = new List<(int expect, int[] prices, int fee)>
+//         {
+//             (6, new[] {1,3,7,5,10,3}, 3),
+//         };
+//         foreach (var test in tests)
+//             Assert.Equal(test.expect, MaxProfit(test.prices, test.fee));
+//     }
+// }
---
+++ LeetCode/480_SlidingWindowMedian.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace SlidingWindowMedian_480;
+
+// TODO 480. Sliding Window Median
+
+// public class Solution
+// {
+//     public double[] MedianSlidingWindow(int[] nums, int k)
+//     {
+//         var result = new List<double>();
+//
+//         var window = new SortedList<int, int>();
+//
+//         var getMiddle = k % 2 != 0;
+//         
+//         for (var index = 0; index < nums.Length; index++)
+//         {
+//             window.Add(nums[index], index);
+//             if (window.Count <= k)
+//                 continue;
+//             var median = getMiddle ? window.Values[k / 2] : window.Values[k / 2] / 2.0 + window.Values[k / 2 + 1] / 2.0;
+//             result.Add(median);
+//             window.RemoveAt(0);
+//         }
+//         return result.ToArray();
+//     }
+//
+//     [Fact]
+//     public void Test()
+//     {
+//         var tests = new List<(double[] expect, int[] nums, int k)>
+//         {
+//             (new[] { 1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000 }, new[] { 1,3,-1,-3,5,3,6,7 }, 3),
+//         };
+//         foreach (var test in tests)
+//             Assert.Equal(test.expect, MedianSlidingWindow(test.nums, test.k));
+//     }
+// }
