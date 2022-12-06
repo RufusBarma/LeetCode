b9076f22e71a6884aa4376a4f4006c9fc18375e8
RufusBarma <neymanyu@gmail.com>
2022-12-06 22:04:00 +0500

    Solve "392. Is Subsequence"
    https://leetcode.com/problems/is-subsequence/description/?envType=study-plan&id=level-1

---
+++ LeetCode/392_IsSubsequence.cs
@@ -0,0 +1,32 @@
+﻿namespace IsSubsequence_392;
+
+// 205. Isomorphic Strings
+
+// "a", "a" = true
+// "a", "b" = false
+// "a", "bab" = true
+// "a", "ba" = true
+// "a", "ab" = true
+// "", "ab" = true
+// "ab", "bab" = true
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0)
+            return true;
+        
+        var currentIndex = 0;
+        foreach (var tChar in t)
+        {
+            if (tChar == s[currentIndex])
+            {
+                currentIndex++;
+                if (currentIndex >= s.Length)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
